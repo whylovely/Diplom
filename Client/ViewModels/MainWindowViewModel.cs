b6d4dd59dfe30e7ca5b6698b9a525a4285867d00
whylovely <viner05@list.ru>
2026-01-19 11:02:17 +0300

    Инициализация клиента

---
+++ Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Client.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ Client/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Client.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
