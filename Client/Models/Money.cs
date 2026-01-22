785b2b64ad1187498a35e3e1f719f8c8f266b1b8
whylovely <viner05@list.ru>
2026-01-22 14:21:18 +0300

    Переделал чуть чуть MOney

--- Client/Models/Money.cs
+++ Client/Models/Money.cs
@@ -4,9 +4,6 @@ namespace Client.Models
 {
     public record class Money
     {
-        public override string ToString()
-        {
-            return $"{Amount:0.##} {CurrencyCode}";
-        }
+        public override string ToString() => $"{Amount:0.##} {CurrencyCode}";
     }
 }
