7ec959131caa90dcae0ea45260bd40b592f1aa8b
whylovely <viner05@list.ru>
2026-01-23 10:46:41 +0300

    Немного доисправил исправления

--- Client/Views/AccountsView.axaml.cs
+++ Client/Views/AccountsView.axaml.cs
@@ -14,12 +14,4 @@ public partial class AccountsView : UserControl
     {
         InitializeComponent();
     }
-
-    private void AddAccount_Click(object? sender, RoutedEventArgs e)
-    {
-        var vm = DataContext as AccountsViewModel;
-
-        // ¬ажно: если vm == null Ч DataContext не тот, и биндинги не работают
-        vm?.Accounts.Insert(0, new Account { Name = "TEST", CurrencyCode = "RUB", Balance = 0 });
-    }
 }
