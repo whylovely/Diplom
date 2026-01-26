826eb8b91766fb02d12e2f6347b4d621b3adc9c1
whylovely <viner05@list.ru>
2026-01-26 17:32:59 +0300

    Добавил Общий итог и итог по месяцам

--- Client/ViewModels/ReportViewModel.cs
+++ Client/ViewModels/ReportViewModel.cs
@@ -14,9 +14,13 @@ namespace Client.ViewModels
 
         public ObservableCollection<CategoryTotalRow> ExpenseRows { get; } = new();
         public ObservableCollection<CategoryTotalRow> IncomeRows { get; } = new();
+        public ObservableCollection<MonthlyTotalRow> MothlyRows { get; } = new();
 
         [ObservableProperty] private DateTimeOffset _dateFrom = DateTimeOffset.Now.AddMonths(-1);
         [ObservableProperty] private DateTimeOffset _dateTo = DateTimeOffset.Now;
+        [ObservableProperty] private decimal _totalExpense;
+        [ObservableProperty] private decimal _totalIncome;
+        [ObservableProperty] private decimal _net;
 
         public ReportViewModel(IDataService data)
         {
@@ -79,6 +83,45 @@ namespace Client.ViewModels
 
             foreach (var row in incomeGroups)
                 IncomeRows.Add(row);
+
+            _totalExpense = ExpenseRows.Sum(r => r.Total); 
+            _totalIncome = IncomeRows.Sum(r => r.Total);
+            _net = _totalIncome - _totalExpense;
+
+            // Расчет помесячных итогов
+            MothlyRows.Clear();
+
+            var mothlyGroups = txInRange
+                .GroupBy(t => new { t.Date.Year, t.Date.Month })
+                .OrderBy(g => g.Key.Year).ThenBy(g => g.Key.Month);
+
+            foreach (var mg in mothlyGroups)
+            {
+                var enteries = mg.SelectMany(t => t.Entries);
+
+                var expense = enteries
+                    .Where(e =>
+                    {
+                        var acc = accountById[e.AccountId];
+                        return acc.Type == AccountType.Expense && e.Direction == EntryDirection.Debit;
+                    })
+                    .Sum(e => e.Amount.Amount);
+
+                var income = enteries
+                    .Where(e =>
+                    {
+                        var acc = accountById[e.AccountId];
+                        return acc.Type == AccountType.Income && e.Direction == EntryDirection.Credit;
+                    })
+                    .Sum(e => e.Amount.Amount);
+
+                MothlyRows.Add(new MonthlyTotalRow
+                {
+                    Month = $"{mg.Key.Year:D4}-{mg.Key.Month:D2}",
+                    Income = income,
+                    Expense = expense
+                });
+            }
         }
     }
 }
